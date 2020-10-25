96ac997dae078969937d0ba51d7f2709a2835758
GersonUmanzor <gbumanzor@outlook.com>
2020-10-25 14:44:38 -0600

    :recycle: Import module and create modal method.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ExampleComponent } from './bay-modal/components/example/example.component';
+import { ModalService } from './bay-modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ngx-custom-modal';
+
+  constructor(private modalService: ModalService) {}
+
+  openModal(): void {
+    const ref = this.modalService.open<ExampleComponent, string>(
+      ExampleComponent,
+      {
+        data: 'I am a dynamic component inside a modal',
+        height: '300px',
+        width: '200px',
+      }
+    );
+
+    ref.afterClosed.subscribe((result) => console.log(result));
+  }
 }
